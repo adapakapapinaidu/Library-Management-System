85a9a8ee9114e989413b91dd9c4574cfce4165e0
adapakapapinaidu <adapakapapinaidu444@gmail.com>
2025-11-17 09:58:27 +0530

    Update WebConfig.java

--- src/main/java/it/objectmethod/demo/spring/config/WebConfig.java
+++ src/main/java/it/objectmethod/demo/spring/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://lms-frontend-five-tau.vercel.app")
+                        .allowedOrigins("https://lms-frontend-kfv1u54id-adapaka-papinaidus-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
