2be3b051ac13fc365284c4216084a5f47cff4c8d
adapakapapinaidu <adapakapapinaidu444@gmail.com>
2025-11-17 10:51:03 +0530

    Update WebConfig.java

--- src/main/java/it/objectmethod/demo/spring/config/WebConfig.java
+++ src/main/java/it/objectmethod/demo/spring/config/WebConfig.java
@@ -7,7 +7,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lms-frontend-nine-neon.vercel.app/")
+                .allowedOrigins("https://lms-frontend-nine-neon.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
