bf0ee1f335e4a2db3b4d6b6cf0490a13aea13ea9
adapakapapinaidu <adapakapapinaidu444@gmail.com>
2025-11-17 13:33:33 +0530

    Update SecurityConfig.java

--- src/main/java/it/objectmethod/demo/spring/config/SecurityConfig.java
+++ src/main/java/it/objectmethod/demo/spring/config/SecurityConfig.java
@@ -1,19 +1,24 @@
+package it.objectmethod.demo.spring.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors() // Enable CORS
+        http.cors()    // Enable CORS
             .and()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/**").permitAll()   // Allow all endpoints
-            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll(); // Allow preflight OPTIONS requests
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // allow preflight requests
+            .antMatchers("/**").permitAll();
         return http.build();
     }
 }
