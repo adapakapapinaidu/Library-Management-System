a115a956f367f9dfecf2bb8a0e373cf4b04b01c3
adapakapapinaidu <adapakapapinaidu444@gmail.com>
2025-11-17 09:58:03 +0530

    Update SecurityConfig.java

--- src/main/java/it/objectmethod/demo/spring/config/SecurityConfig.java
+++ src/main/java/it/objectmethod/demo/spring/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:3000",
-            "https://lms-frontend-five-tau.vercel.app"
+            "https://lms-frontend-kfv1u54id-adapaka-papinaidus-projects.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
