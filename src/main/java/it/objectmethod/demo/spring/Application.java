a1749d1b64b62b248e2676de75768e2b275ec415
adapakapapinaidu <adapakapapinaidu444@gmail.com>
2026-03-02 09:02:28 +0000

    temporary disable database

--- src/main/java/it/objectmethod/demo/spring/Application.java
+++ src/main/java/it/objectmethod/demo/spring/Application.java
@@ -3,8 +3,12 @@ package it.objectmethod.demo.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableJpaRepositories("it.objectmethod.demo.spring.repository")
+@EntityScan("it.objectmethod.demo.spring.models")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/it/objectmethod/demo/spring/repository/MemberRepository.java
+++ src/main/java/it/objectmethod/demo/spring/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package it.objectmethod.demo.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import it.objectmethod.demo.spring.models.MemberObject;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberObject, Long> {
 
 }
